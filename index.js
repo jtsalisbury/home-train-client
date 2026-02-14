3427d389370fd70dbd450135482f8d090f0717d4
JT Salisbury <crazyscouter1@gmail.com>
2026-02-14 12:27:16 -0500

    Fix

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ const fillCircle = (cx, cy, r) => {
     fillCircle(cx, cy, r);
   
     matrix
-      .fgColor(0xE0E0E0)
+      .fgColor(0xFFFFFF)
       .bgColor(0x000000)
       .drawText(train.train_type, cx - 2, cy - 3);
   };
