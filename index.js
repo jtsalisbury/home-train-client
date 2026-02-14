4a72a9559a2c216d9a6f0fd520eb7fbcb69f2d75
sexybrowntwink <angelocalayagjobs@gmail.com>
2026-02-14 11:57:27 -0500

    fix

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ const renderArrivalTimes = (trains) => {
 
             .fgColor(0xFFFFFF)
             .bgColor(0x000000)
-            .drawText(trains[0].train_type, 3, 4);
+            .drawText(trains[0].train_type, 6, 7);
     }
    
     matrix.sync();
